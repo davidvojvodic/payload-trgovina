1bf816e43232101e3bb9978cafcf523d3b0265de
David Vojvodić <davidvojvodic1@gmail.com>
2023-12-24 20:24:02 +0100

    hero

---
+++ src/app/_components/Promotion/index.tsx
@@ -0,0 +1,40 @@
+'use client'
+import React, { useState } from 'react'
+
+import classes from './index.module.scss'
+
+const Promotion = () => {
+  const [time, setTime] = useState({
+    days: 0,
+    hours: 0,
+    minutes: 0,
+    seconds: 0,
+  })
+
+  const targetDate = new Date()
+
+  return (
+    <section className={classes.promotion}>
+      <div className={classes.textBox}>
+        <h3 className={classes.title}>Deals of the Month</h3>
+        <p>Get ready for shopping experience with our Deals of the Month!</p>
+
+        <ul className={classes.stats}>
+          <StatBox label="Days" value={time.days} />
+          <StatBox label="Hours" value={time.hours} />
+          <StatBox label="Minutes" value={time.minutes} />
+          <StatBox label="Seconds" value={time.seconds} />
+        </ul>
+      </div>
+    </section>
+  )
+}
+
+const StatBox = ({ label, value }: { label: string; value: number }) => (
+  <li className={classes.statBox}>
+    <h4>{value}</h4>
+    <p>{label}</p>
+  </li>
+)
+
+export default Promotion
